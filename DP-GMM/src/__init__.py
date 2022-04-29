d03336af99d59ca993ec4360070f0a6d4a28ff3b
Charlie Harrington <charlesharrington@g.harvard.edu>
2022-04-29 15:56:03 -0400

    Removing unnecessary packaging

--- DP-GMM/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._base_gmm import GMM
-
-
-__all__ = ["GMM"]
--- src/test_gmm.py
+++ src/test_gmm.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from src import GMM
+from base_gmm import GMM
 
 test_dat = np.random.randint(1, 10, size=(20, 2))
 gmm1 = GMM(2)
@@ -14,7 +14,7 @@ try:
 except ValueError as e:
     print(e)
 
-faithful = pd.read_csv("../data/faithful.csv")
+faithful = pd.read_csv("data/faithful.csv")
 dat = faithful["waiting"].values.reshape(-1, 1)
 gmm = GMM(2).fit(dat)
 gmm.plot_mixture(60)
